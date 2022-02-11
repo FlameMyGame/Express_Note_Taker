ddf2dea4a547573081657ca7cc3347307d95bd81
Dawson Gunnell <dlgunnell@outlook.com>
2022-02-11 08:57:25 -0700

    added server.js to add routers, a listener, PORT setup, local express usage. API routes and html routes added aswell.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Sets up the Express app for data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// ROUTER
+
+require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app);
+
+
+// LISTENER
+
+app.listen(PORT, function() {
+    console.log(`Server is listening on PORT: ${PORT}`);
+})
